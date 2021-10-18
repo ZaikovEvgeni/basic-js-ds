9c972f236034d54080caa00a41dda9b0974a22f6
Zaikov Evgeni <evgeniikas9@gmail.com>
2021-10-18 22:11:00 +0300

    feat: did desqueue

--- src/st-queue.js
+++ src/st-queue.js
@@ -15,19 +15,60 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 module.exports = class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  
   }
 
-  enqueue(/* value */) {
+
+  getUnderlyingList() {
+
+    if(this.head) {
+      let current = this.head;
+
+      while(current.next) {
+        current = current.next;
+      }
+
+      return current;
+
+    } else {
+      return null
+    }
+
+
+   
+  }
+
+  enqueue(/* value*/ ) {
+    // if(this.length === 0) {
+    //   this.head = new Queue(value);
+    // } else {
+    //   let current = this.head;
+
+    //   while(current.next) {
+    //     current = current.next;
+    //   }
+    //   current.next = new Queue(value);
+    // }
+
+    // this.length++;
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    
+    if(this.head) {
+      let current = this.head.value;
+      this.head = this.head.next;
+      return current;
+    }
+    return null;
+   
+
   }
 
 }
